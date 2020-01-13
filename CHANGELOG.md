2dc73ad48733227d3eb25caf32c335df07aeeb3a
Alfredo Di Napoli <alfredo.dinapoli@gmail.com>
2020-01-13 12:07:48 +0100

    go

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for circle-ci-git-submodules
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ Main.hs
@@ -0,0 +1,17 @@
+module Main where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import MySubmodule (exposed)
+
+main :: IO ()
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [unitTests]
+
+unitTests = testGroup "Unit tests"
+  [ testCase "Exposed" $
+      exposed `compare` 42 @?= EQ
+  ]
